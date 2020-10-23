bbbdd2061f6bb991d9ac6ad8f95a6f7c9070e004
hectorcoronado <hectorcoronado.developer@gmail.com>
2020-10-22 23:53:29 -0700

    code for e.2 by kcd

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -1,40 +1,74 @@
 // useEffect: persistent state
 // http://localhost:3000/isolated/exercise/02.js
 
-import React, {useEffect, useState} from 'react'
+import React, {useEffect, useRef, useState} from 'react'
 
-const useLocalStorage = (key, initialValue) => {
-  const [storedValue, setStoredValue] = useState(() => {
-    try {
-      const item = window.localStorage.getItem(key)
-      return item ? JSON.parse(item) : initialValue
-    } catch (err) {
-      console.warn('Error setting localStorage:', err)
-      return initialValue
-    }
-  })
+// const useLocalStorage = (key, initialValue = '') => {
+//   const [storedValue, setStoredValue] = useState(() => {
+//     try {
+//       const item = window.localStorage.getItem(key)
+//       return item ? JSON.parse(item) : initialValue
+//     } catch (err) {
+//       console.warn('Error setting localStorage:', err)
+//       return initialValue
+//     }
+//   })
 
-  const setValue = value => {
-    try {
-      const valueToStore = 
-        value instanceof Function
-          ? value(storedValue)
-          : value
-      setStoredValue(valueToStore)
-      window.localStorage.setItem(key, JSON.stringify(valueToStore))
-    } catch (err) {
-      console.error(err)
-    }
-  }
-  return [storedValue, setValue]
+//   const setValue = value => {
+//     try {
+//       const valueToStore = 
+//         value instanceof Function
+//           ? value(storedValue)
+//           : value
+//       setStoredValue(valueToStore)
+//       window.localStorage.setItem(key, JSON.stringify(valueToStore))
+//     } catch (err) {
+//       console.error(err)
+//     }
+//   }
+//   return [storedValue, setValue]
+// }
+
+const useLocalStorageState = (
+  key,
+  defaultValue = '',
+  {
+    serialize = JSON.stringify,
+    deserialize = JSON.parse
+  } = {}) => {
+    const [state, setState] = useState(
+      () => {
+        const valueInLocalStorage = window.localStorage.getItem(key)
+
+        if (valueInLocalStorage) {
+          return deserialize(valueInLocalStorage)
+        }
+
+        return typeof defaultValue === 'function' ? defaultValue() : defaultValue
+      }
+    )
+
+    // keep track of key in case we need to change
+    // how value is stored in localStorage
+    const prevKeyRef = useRef(key)
+
+    useEffect(() => {
+      const prevKey = prevKeyRef.current
+
+      if (prevKey !== key) {
+        window.localStorage.removeItem(prevKey)
+      }
+
+      prevKeyRef.current = key
+
+      window.localStorage.setItem(key, serialize(state))
+    }, [key, serialize, state])
+
+    return [state, setState]
 }
 
 const Greeting = ({initialName = ''}) => {
-  const [name, setName] = useLocalStorage('name', initialName)
-
-  useEffect(() => {
-    setName(name)
-  }, [name, setName])
+  const [name, setName] = useLocalStorageState('name', initialName)
 
   const handleChange = (event) => {
     setName(event.target.value)
@@ -44,7 +78,7 @@ const Greeting = ({initialName = ''}) => {
     <div>
       <form>
         <label htmlFor="name">Name: </label>
-        <input onChange={handleChange} id="name" />
+        <input value={name} onChange={handleChange} id="name" />
       </form>
       {name ? <strong>Hello {name}</strong> : 'Please type your name'}
     </div>
